1fe0c8567d5db29d9f30721350ae2517c8087a07
cibkol <cibkoll@yandex.ru>
2022-07-10 09:52:36 +0500

    init

---
+++ README.md
@@ -0,0 +1,9 @@
+Тестовое задание assignment. 
+
+Для запуска приложения 
+1. yarn install
+2. yarn start
+
+Для билда 
+1. yarn install
+2. yarn build
---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import { observer } from "mobx-react-lite";
+import React from "react";
+
+const App: React.FC = () => {
+  return <div className="App"></div>;
+};
+
+export default observer(App);
