00d7155f42ffcc1a0d0522129297d0f20c594ed8
cibkol <cibkoll@yandex.ru>
2022-07-12 05:24:12 +0500

    Привел к нужному виду и шрифты

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
 import { observer } from "mobx-react-lite";
 import React from "react";
+import TextField from "./components/TextField";
+import "./assets/sass/global.sass";
 
 const App: React.FC = () => {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <TextField
+        onChange={function (value: string, key: string): void {
+          throw new Error("Function not implemented.");
+        }}
+        value={""}
+        name={""}
+      />
+    </div>
+  );
 };
 
 export default observer(App);
